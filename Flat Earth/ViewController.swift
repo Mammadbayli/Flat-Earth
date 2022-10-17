2c1672c2d15bc6759fdcd017c28105b3650d1244
Javad <javad@mammadbayli.com>
2022-10-16 22:49:28 -0700

    initial commit

---
+++ Flat Earth/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flat Earth
+//
+//  Created by Javad Mammadbayli on 10/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .red
+    }
+
+
+}
+
